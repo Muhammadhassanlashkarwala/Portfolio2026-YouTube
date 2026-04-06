5a036757fc36737c4b24ae459e9a79408ae88ce6
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-04-06 16:46:18 +0500

    Portfolio complete!

---
+++ script.js
@@ -0,0 +1,36 @@
+const filterButtons = document.querySelectorAll(".filter-btn");
+const projectCards = document.querySelectorAll(".project-card");
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () =>{
+
+        filterButtons.forEach(btn => btn.classList.remove("active"));
+
+        button.classList.add("active");
+        const filterValue = button.getAttribute("data-filter");
+
+        projectCards.forEach(card => {
+            const category = card.getAttribute("data-category");
+
+            if(filterValue === "all" || category === filterValue){
+                card.style.display = "block";
+            }else{
+                card.style.display = "none";
+            }
+        });
+    });
+});
+
+// Contact Form alert
+
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function(e){
+e.preventDefault();
+ Swal.fire({
+    title: "Message sent Successfully!",
+    icon: "success",
+    draggable: true
+});
+// alert("Message Sent")
+});
